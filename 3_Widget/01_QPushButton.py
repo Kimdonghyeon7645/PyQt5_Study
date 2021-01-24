78807f1e4e63d0f8499ef243ac1007d0c14baa4e
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-01-24 16:46:05 +0900

    code : add 3/01

---
+++ 3_Widget/01_QPushButton.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        btn1 = QPushButton('&Button1', self)
+        btn1.setCheckable(True)
+        btn1.toggle()
+
+        btn2 = QPushButton(self)
+        btn2.setText('Button&2')
+
+        btn3 = QPushButton('Button3', self)
+        btn3.setEnabled(False)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(btn1)
+        vbox.addWidget(btn2)
+        vbox.addWidget(btn3)
+
+        self.setLayout(vbox)
+        self.setWindowTitle('QPushButton')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
